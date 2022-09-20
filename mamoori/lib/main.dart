899531211314950b26261185b452691fc7b7e8cd
kimeunyeong <zerokim0215@gmail.com>
2022-09-21 00:53:09 +0900

    fix(will_provider): 패키지추가&main화면 초기세팅;

--- mamoori/lib/main.dart
+++ mamoori/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mamoori/provider/wills.dart';
 import 'package:mamoori/view/home_view.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,13 +11,14 @@ class MyApp extends StatelessWidget {
   static final String title = 'MAMOORI';
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: Colors.orange[300],
-          scaffoldBackgroundColor: Colors.white),
-      home: HomeView(title: title),
-    );
-  }
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+    create: ((context) => WillsProvider(),
+    child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              primaryColor: Colors.orange[300],
+              scaffoldBackgroundColor: Colors.white),
+          home: HomeView(title: title),
+        ),
+  );
 }
