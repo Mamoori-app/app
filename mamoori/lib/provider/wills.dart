8390f4d41cdee43081bc8d151329f73ae11fdbc3
kimeunyeong <zerokim0215@gmail.com>
2022-09-21 00:51:10 +0900

    fix(will_provider): add/delete 함수추가;

---
+++ mamoori/lib/provider/wills.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:mamoori/model/will.dart';
+
+class WillsProvider extends ChangeNotifier {
+  List<Will> _willList = [
+    Will(id: 1, title: 'First', content: '내용입니당', createdTime: DateTime.now()),
+  ];
+}
