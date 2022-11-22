138b77a125328b8488831e946b644ea39f67e66d
kimeunyeong <zerokim0215@gmail.com>
2022-11-23 04:02:35 +0900

    fix: model, repository 재구성

---
+++ mamoori/lib/data/repository/will_repository_impl.dart
@@ -0,0 +1,34 @@
+import 'package:mamoori/domain/model/will.dart';
+import 'package:mamoori/repository/will_repository.dart';
+
+class WillRepositoryImpl implements WillRepository {
+  @override
+  Future<void> deleteWill(Will will) {
+    // TODO: implement deleteWill
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Will> getWillById(int id) {
+    // TODO: implement getWillById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Will>> getWills() {
+    // TODO: implement getWills
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> insertWill(Will will) {
+    // TODO: implement insertWill
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateWill(Will will) {
+    // TODO: implement updateWill
+    throw UnimplementedError();
+  }
+}
