fea36e79a32c7e42232bcc21c395de976d5bf78d
kimeunyeong <zerokim0215@gmail.com>
2023-01-05 15:55:59 +0900

    feat: kakao_login-inital_setup

--- mamoori/lib/main.dart
+++ mamoori/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:mamoori/di/provider_setup.dart';
 import 'package:mamoori/presentation/wills/wills_view.dart';
-import 'package:mamoori/ui/home_view.dart';
 import 'package:provider/provider.dart';
-
+import 'api/api_key.dart';
 import 'ui/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  KakaoSdk.init(nativeAppKey: kakaoNativeKey);
   final providers = await getProviders();
   runApp(
     MultiProvider(
